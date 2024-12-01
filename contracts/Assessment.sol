487710a0afbbe34b6cf56cfeee0195f496f911b5
RC <202111443@fit.edu.ph>
2024-12-01 18:52:37 +0800

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -4,82 +4,27 @@ pragma solidity ^0.8.9;
 //import "hardhat/console.sol";
 
 contract Assessment {
-    address payable public owner;
-    uint256 public balance;
+    string public message;
+    uint256 public count;
 
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
-    event WithdrawAll(uint256 amount);
-
-    constructor(uint initBalance) payable {
-        owner = payable(msg.sender);
-        balance = initBalance;
+    // Constructor to initialize the contract
+    constructor(string memory initialMessage) {
+        message = initialMessage;
+        count = 0;
     }
 
-    function getBalance() public view returns(uint256){
-        return balance;
+    // Function to update the message
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
     }
 
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
-        balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
-        emit Deposit(_amount);
+    // Function to increment the counter
+    function incrementCounter() public {
+        count++;
     }
 
-    // custom error
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
-
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
-        }
-
-        // withdraw the given amount
-        balance -= _withdrawAmount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
-        emit Withdraw(_withdrawAmount);
-    }
-
-    function withdrawAll(uint256 _Burnamount) public {
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
-
-        // withdraw all until balance is 0
-        while (balance > 0) {
-            uint256 withdrawalAmount = _Burnamount;
-
-            // if the withdrawal amount is more than the balance, adjust it
-            if (withdrawalAmount > balance) {
-                withdrawalAmount = balance;
-            }
-
-            // withdraw the given amount
-            balance -= withdrawalAmount;
-
-            // emit the event for each withdrawal
-            emit WithdrawAll(withdrawalAmount);
-        }
-
-        // assert the balance is correct
-        assert(balance == 0);
+    // Function to get the current counter value (optional, as `count` is public)
+    function getCount() public view returns (uint256) {
+        return count;
     }
 }
